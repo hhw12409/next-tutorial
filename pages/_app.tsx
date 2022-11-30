bc191b9c6c1c35cc13045faee804bd4576d77b8e
hhw12409 <sksk2425@naver.com>
2022-12-01 01:57:50 +0900

    feat: sass setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import "semantic-ui-css/semantic.min.css";
 import Footer from "../src/components/Footer";
 import Top from "../src/components/Top";
+import styles from "../styles/Home.module.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
