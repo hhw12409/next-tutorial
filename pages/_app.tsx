74559677e03982fc5461113770f1a7a150545361
hhw12409 <sksk2425@naver.com>
2022-12-01 02:09:33 +0900

    style: styling layout and logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,11 @@ import type { AppProps } from "next/app";
 import "semantic-ui-css/semantic.min.css";
 import Footer from "../src/components/Footer";
 import Top from "../src/components/Top";
-import styles from "../styles/Home.module.scss";
+import styles from "../styles/_app.module.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <div className={styles.container}>
       <Top />
       <Component {...pageProps} />
       <Footer />
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Header } from "semantic-ui-react";
 import Gnb from "./Gnb";
+import styles from "../../styles/Top.module.scss";
 
 export default function Top() {
   return (
-    <div>
-      <Image src={"/images/logo.png"} alt="logo" width={100} height={100} />
-      <Header as="h1">Nextjs Practice</Header>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Image src={"/images/logo.png"} alt="logo" width={100} height={100} />
+        <Header as="h1">Nextjs Practice</Header>
+      </div>
       <Gnb />
     </div>
   );
