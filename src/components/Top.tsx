52b16de937f7f504a4d44462cc66e8d457e1a925
hhw12409 <sksk2425@naver.com>
2022-12-01 01:47:00 +0900

    docs: add width and height

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -4,7 +4,7 @@ import { Header } from "semantic-ui-react";
 export default function Top() {
   return (
     <div>
-      <Image src={"/images/logo.png"} alt="logo" />
+      <Image src={"/images/logo.png"} alt="logo" width={100} height={100} />
       <Header as="h1">Nextjs Practice</Header>
     </div>
   );
