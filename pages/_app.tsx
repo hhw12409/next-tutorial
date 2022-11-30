969c1f14f124f9ca818d31954148603c1d3d4707
hhw12409 <sksk2425@naver.com>
2022-12-01 01:45:31 +0900

    feat: attach Top component at _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "semantic-ui-css/semantic.min.css";
 import Footer from "../src/components/Footer";
+import Top from "../src/components/Top";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Top />
       <Component {...pageProps} />
       <Footer />
     </div>
