5ab4ae54fd791ad69f3bdd6505c348e1693a8891
hhw12409 <sksk2425@naver.com>
2022-12-01 01:38:09 +0900

    Feat: setting semantic ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "semantic-ui-css/semantic.min.css";
+import Footer from "../src/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
