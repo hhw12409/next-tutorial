517a89542f40232ae1b547c726f471636dbe445c
hhw12409 <sksk2425@naver.com>
2022-12-01 01:40:29 +0900

    feat: make header

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Header } from "semantic-ui-react";
 
 export default function Top() {
   return (
     <div>
       <Image src={"/images/logo.png"} alt="logo" />
+      <Header as="h1">Nextjs Practice</Header>
     </div>
   );
 }
