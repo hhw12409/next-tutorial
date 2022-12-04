1d979240f1ef7f54af617786dd572af408adb591
hhw12409 <sksk2425@naver.com>
2022-12-05 01:15:53 +0900

    feat: detail page api setting

--- pages/view/[id].tsx
+++ pages/view/[id].tsx
@@ -1,7 +1,26 @@
 import { useRouter } from "next/router";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Post() {
   const router = useRouter();
   const { id } = router.query;
+  const [item, setItem] = useState({});
+
+  function getData() {
+    const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+    axios.get(API_URL).then((res) => {
+      if (res.status === 200) {
+        setItem(res.data);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (id && Number(id) > 0) {
+      getData();
+    }
+  }, [id]);
+
   return <p>Post: {id}</p>;
 }
