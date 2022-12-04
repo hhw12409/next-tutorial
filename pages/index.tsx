9b424fe46752bae9eb4cfa36156bc2212cc985bc
hhw12409 <sksk2425@naver.com>
2022-12-04 23:28:36 +0900

    api setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import axios from "axios";
 import Head from "next/head";
-import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [list, setList] = useState([]);
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+  function getData() {
+    axios.get(API_URL).then((res) => setList(res.data));
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
       <Head>
