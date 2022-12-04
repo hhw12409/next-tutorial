a38e7ea3dc54c9c793353a7e4fea12f2406d439d
hhw12409 <sksk2425@naver.com>
2022-12-05 01:28:20 +0900

    feat: add image remotePattern

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d3t32hsnjxo7q6.cloudfront.net",
+        port: "",
+        pathname: "/i/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
