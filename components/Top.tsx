52a3f50a42aaeb39131a5cb3a4c79e6f4c61c912
hhw12409 <sksk2425@naver.com>
2022-12-08 02:23:39 +0900

    docs: add priority option

--- components/Top.tsx
+++ components/Top.tsx
@@ -7,7 +7,13 @@ export default function Top() {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <Image src={"/vercel.svg"} alt="logo" width={100} height={100} />
+        <Image
+          src={"/vercel.svg"}
+          alt="logo"
+          width={100}
+          height={100}
+          priority={true}
+        />
         <Header as="h1">Nextjs Practice</Header>
       </div>
       <Gnb />
