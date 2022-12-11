2176216dcda5b1711cd7649f1b7d42a4f83a218c
hhw12409 <sksk2425@naver.com>
2022-12-12 01:28:22 +0900

    feat: 404 page

---
+++ pages/_error.tsx
@@ -0,0 +1,16 @@
+export default function Error({ statusCode }) {
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : "An error occurred on client"}
+    </p>
+  );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+// 서버에러 페이지는 정적으로 최적화를 제공하지않는다.
