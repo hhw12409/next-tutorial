34c0479d09d74fc48fcdb311efd0a4005c6a1993
hhw12409 <sksk2425@naver.com>
2022-12-12 01:42:40 +0900

    feat: 500 page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,4 +1,5 @@
-export default function Error({ statusCode }) {
+import type { NextApiResponse } from "next";
+export default function Error({ statusCode }: { statusCode: string }) {
   return (
     <p>
       {statusCode
@@ -8,9 +9,10 @@ export default function Error({ statusCode }) {
   );
 }
 
-Error.getInitialProps = ({ res, err }) => {
+Error.getInitialProps = ({ res, err }: { res: NextApiResponse; err: any }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };
 };
 
 // 서버에러 페이지는 정적으로 최적화를 제공하지않는다.
+// 해당코드로 404, 500모두 처리가 가능하다. 하지만 404는 static로 관리하는것이 좋다.
